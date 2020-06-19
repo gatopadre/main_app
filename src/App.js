fcbc876351fa48aad22961b3b1fc36b74de809cd
Seba Zuniga <sebastianzunigasaavedra@gmail.com>
2020-06-18 23:56:19 -0400

    subiendo avance para dejar todo el template con react, solo faltaria eliminar la div seba falsa, hay que pensarlo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import Wrapper from './components/Wrapper'
+import Wrapper from './components/Wrapper';
+import ScrollToTop from './components/ScrollToTop';
+import LogoutModal from './components/LogoutModal';
 
 function App() {
   return (
+    // es falsa despues hay que quitar la div "seba"
     <div className="seba">
       <Wrapper />
+      <ScrollToTop/>
+      <LogoutModal/>
     </div>
   );
 }
